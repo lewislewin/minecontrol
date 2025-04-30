c4a0c417861de71e12fce16cd1f4c5eb7dfbd887
Lewis Lewin <lewis@lewislewin.com>
2025-04-30 20:20:47 +0100

    Able to start server using button

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,42 @@
+import type { Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { env } from '$env/dynamic/private';
+
+import { EC2 } from '@aws-sdk/client-ec2';
+
+export const load = (async () => {
+    return {};
+}) satisfies PageServerLoad;
+
+export const actions = {
+    default: async ({request}) => {
+        const data = await request.formData()
+        const password = data.get('password')
+
+        if (password !== env.PRIVATE_SECRET_PASSWORD) {
+            return { success: false, error: 'bad password'}
+        }
+
+        const instanceId = 'i-037dd598360a632ae'
+
+        const ec2 = new EC2({
+            region: env.PRIVATE_AWS_REGION,
+
+            credentials: {
+                accessKeyId: env.PRIVATE_AWS_ACCESS_KEY,
+                secretAccessKey: env.PRIVATE_AWS_ACCESS_KEY_SECRET
+            }
+        })
+
+        try {
+            let ids: string[] = []
+            ids = [instanceId]
+
+            await ec2.startInstances({InstanceIds: ids})
+
+            return { success: true, started: ids }
+        } catch (err) {
+            return { success: false, error: (err as Error).message }
+        }
+    }
+} satisfies Actions
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { PageProps } from "./$types";
+
+    let { form }: PageProps = $props()
+</script>
+
+<form method="post">
+    <label>
+        Password
+        <input type="password" name="password" id="password">
+    </label>
+    <button>
+        Start Server
+    </button>
+
+    {#if form?.error}
+    <p>{form.error}</p>
+    {/if}
+</form>
