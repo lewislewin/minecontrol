def241e929ff947dec766defdc9035f4f6c8eddc
Lewis Lewin <lewis@lewislewin.com>
2025-05-05 16:30:52 +0100

    Add current status to front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,7 @@
 <div class="mx-auto mt-12 max-w-lg rounded-2xl bg-white p-6 shadow">
 	<h1 class="mb-4 text-2xl font-semibold">Start MC Server</h1>
 	<p>Current IP: { serverIp }</p>
+	<p>Current Status: { currentStatus }</p>
 	<form
 		method="post"
 		use:enhance={() => {
