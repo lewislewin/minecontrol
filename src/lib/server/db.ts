df38d86f33febbb2ad934c62c9b7e566b19ed161
Lewis Lewin <lewis@lewislewin.com>
2025-05-06 21:51:31 +0100

    Removed console

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,9 +1,8 @@
 // lib/server/services/db.ts
-import { error } from 'console'
 import { drizzle } from 'drizzle-orm/d1'
 export function getDb(env: App.Platform['env'] | undefined) {
     if (!env) {
-        throw error(500, "Env is not defined")
+        throw new Error('ENV NOT DEFINED')
     }
     return drizzle(env.DB)
 }
