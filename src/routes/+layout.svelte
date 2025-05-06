533a306ab42af7d5024e1fed21ae1ea565e767e8
Lewis Lewin <lewis@lewislewin.com>
2025-05-06 18:04:54 +0100

    Reworked layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,10 @@
 <script lang="ts">
-	import '../app.css';
+    import '../app.css';
 
-	let { children, data } = $props();
+    import type { Snippet } from 'svelte';
+    import type { LayoutData } from './$types';
+
+    let { data, children }: { data: LayoutData, children: Snippet } = $props();
 </script>
 
-<div class="navbar bg-base-100 shadow-sm">
-	<div class="flex-1">
-		<a href="/" class="btn btn-ghost text-xl">minecontrol</a>
-	</div>
-	<div class="flex-none">
-		<ul class="menu menu-horizontal px-1">
-			<li><a href="/user/{data.user.id}">Profile</a></li>
-		</ul>
-	</div>
-</div>
-
-<div class="m-5">
-	{@render children()}
-</div>
+{@render children()}
